3d6ca7990a16b88c27d5d466145bdc2e7b0dde1a
Eduardo Ramirez <edyr94@gmail.com>
2025-09-14 17:02:32 -0700

    dont poll in lock API call

--- src/DweloAPI.ts
+++ src/DweloAPI.ts
@@ -74,14 +74,6 @@ export class DweloAPI {
       method: 'POST',
       data: { 'command': locked ? 'lock' : 'unlock' },
     });
-
-    const target = locked ? 'locked' : 'unlocked';
-    await poll({
-      requestFn: () => this.sensors(id),
-      stopCondition: s => s.find(s => s.sensorType === 'lock')?.value === target,
-      interval: 5000,
-      timeout: 60 * 1000,
-    });
   }
 
   private async request<T>(
@@ -100,43 +92,4 @@ export class DweloAPI {
     });
     return response;
   }
-}
-
-function poll<T>({ requestFn, stopCondition, interval, timeout }: {
-  requestFn: () => Promise<T>;
-  stopCondition: (response: T) => boolean;
-  interval: number;
-  timeout: number;
-}): Promise<T> {
-  let stop = false;
-
-  const executePoll = async (resolve: (r: T) => unknown, reject: (e: Error) => void) => {
-    const result = await requestFn();
-
-    let stopConditionalResult: boolean;
-    try {
-      stopConditionalResult = stopCondition(result);
-    } catch (e) {
-      reject(e as Error);
-      return;
-    }
-
-    if (stopConditionalResult) {
-      resolve(result);
-    } else if (stop) {
-      reject(new Error('timeout'));
-    } else {
-      setTimeout(executePoll, interval, resolve, reject);
-    }
-  };
-
-  const pollResult = new Promise<T>(executePoll);
-  const maxTimeout = new Promise<T>((_, reject) => {
-    setTimeout(() => {
-      reject(new Error('Exceeded max timeout'));
-      stop = true;
-    }, timeout);
-  });
-
-  return Promise.race([pollResult, maxTimeout]);
 }
